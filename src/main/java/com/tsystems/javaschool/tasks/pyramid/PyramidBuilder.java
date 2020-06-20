05a60fc91cd8ed4f7304c27222575116c24f32d8
SergeyAminov <aminov_yar@mail.ru>
2020-06-20 14:31:54 +0300

    add solution of the PyramidBuilder task

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -13,9 +13,73 @@ public class PyramidBuilder {
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
-    }
 
+        /* ---- Check if the size of array matches for pyramid building ---- */
+
+        /* And count rows and columns number */
+        int rows = 0;
+        int elementsInRow = 1;
+        int inputNumbersLength = inputNumbers.size();
+
+        while (inputNumbersLength > 0){
+            inputNumbersLength -= elementsInRow;
+            rows++;
+            elementsInRow++;
+        }
+
+        /* columns = 2 * (elementsInRow - 1) - 1 = 2 * elementsInRow - 3 */
+        int columns = 2 * elementsInRow - 3;
+
+        /* ---- Exceptional cases ---- */
+
+        /* If one of the elements is a null */
+        for (Integer inputNumber : inputNumbers) {
+            if (inputNumber == null)
+                throw new CannotBuildPyramidException();
+        }
+
+        /* If size of array doesn't matches */
+        if (inputNumbersLength < 0)
+            throw new CannotBuildPyramidException();
+
+        /* ---- Sort of given array ---- */
+        inputNumbersLength = inputNumbers.size();
+        for (int i = 0; i < inputNumbersLength-1; i++){
+            for (int j = i+1; j < inputNumbersLength; j++){
+                if (inputNumbers.get(i) > inputNumbers.get(j)){
+                    int cash = inputNumbers.get(i);
+                    inputNumbers.set(i, inputNumbers.get(j));
+                    inputNumbers.set(j, cash);
+                }
+            }
+        }
+
+        /* ---- Filling out an output array ---- */
+
+        int[][] answer = new int[rows][columns];
+
+        /* At first with zeros */
+        for(int i = 0; i < rows; i++)
+            for(int j = 0; j < columns; j++)
+                answer[i][j] = 0;
+
+        /* And then with pyramid values */
+        elementsInRow = 1;
+        int startIndex = columns / 2;
+        int currentInputNumbersIndex = 0;
+
+        for (int rowIndex = 0; rowIndex < rows; rowIndex++){
+            int currentIndex = startIndex;
+            for (int j = 0; j < elementsInRow; j++){
+                answer[rowIndex][currentIndex] = inputNumbers.get(currentInputNumbersIndex);
+                currentInputNumbersIndex++;
+                currentIndex += 2;
+            }
+            startIndex--;
+            elementsInRow++;
+        }
+
+        return answer;
+    }
 
 }
